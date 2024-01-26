72c97fcfe54464e15f0833a270a33733566c2f33
shiffon7343 <game-mu-ka-mi-family@docomo.ne.jp>
2024-01-26 14:21:50 +0900

    add

---
+++ 20231108/TaskB.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <random>
+
+int main()
+{
+    const int arraySize = 10;
+
+    int myArray[arraySize] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+
+    std::shuffle(std::begin(myArray), std::end(myArray),g);
+    
+    std::cout << "Array after shuffle";
+    for (int i = 0; i < arraySize; ++i)
+    {
+        std::cout << myArray[i] << " ";
+    }
+
+    return 0;
+}
